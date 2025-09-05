b5883018399f1d1a31f146b2b4498a026cc3dcfd
12AhsanAli <ahsanmaqsood319@gmail.com>
2025-09-05 11:14:03 +0500

    Initial commit with project files

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import TextEditor from './Components/TextEditor';
+// import TextEditor from './Components/TextEditor';
 import TextEditorReturn from './Components/TextEditorReturns';
 import 'froala-editor/css/froala_editor.pkgd.min.css';
 import 'froala-editor/css/froala_style.min.css';
 import 'froala-editor/css/plugins.pkgd.min.css';
 import 'froala-editor/css/themes/gray.min.css'; 
 import 'froala-editor/js/plugins.pkgd.min.js';
-import EditorWithPreview from './Components/EditorWithPreview';
+// import EditorWithPreview from './Components/EditorWithPreview';
 
 function App() {
 
